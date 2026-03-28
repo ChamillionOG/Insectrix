25540f3070441691766ab59012128c061cd0ceb3
Chamillion <chamillionog@gmail.com>
2026-03-27 20:49:35 -0400

    Deleted Old Program Files

--- main.py
+++ main.py
@@ -3,192 +3,309 @@
 #[---------------]#
 
 import pygame
+import json
 
-from dictionaries.containerDictionaries import container_dictionaries
-from dictionaries.enviromentDictionaries import enviroment_dictionaries
-from upgradeButtons import UpgradeManager, UpgradeButton
+from environmentHandler import EnvironmentManager
 from containerHandler import ContainerManager
-from popupHandler import PopupTextManager
-from bugnetManager import BugnetManager
-from bugHandler import BugManager, Bug
-from phoneManager import PhoneManager
-from savesManager import load_data
+from bugnetHandler import BugnetManager
+from bugHandler import Bug, BugManager
+from popupHandler import PopupText
 
 #[------------]#
 #[----DATA----]#
 #[------------]#
 
-data = load_data()
+from savesManager import load_game, save_game
 
 default_data = {
     "bugs": 0,
     "max_bugs": 3,
-    "spawn_rate": 1,
+    "spawn_rate": 5,
     "currency": 0,
     "bugnet": "wooden",
-    "enviroment": "forest",
-    "sellPlan": "free",
+    "environment": "forest",
+    "sell_plan": "free",
     "container": {
         "type": "small_jar",
         "capacity": 10,
         "offset": 15,
         "bugs": {}
     },
+    "settings": {
+        "sound_effects": True,
+        "popups": True,
+        "music": True
+    },
     "purchases": {}
 }
 
-print(f"Save Data: {data}")
+data = load_game(default_data)
 
-def main():
+with open("dictionaries/bugDictionaries.json", "r") as f:
+    bugs_list = json.load(f)
 
-    #[-------------]#
-    #[----SETUP----]#
-    #[-------------]#
+with open("dictionaries/bugnetDictionaries.json", "r") as f:
+    bugnets_list = json.load(f)
 
-    pygame.init()
-    pygame.font.init()
+with open("dictionaries/environmentDictionaries.json", "r") as f:
+    environments_list = json.load(f)
 
-    screen = pygame.display.set_mode((0, 0), pygame.NOFRAME)
-    screen_width, screen_height = screen.get_size()
-    pygame.display.set_caption("Insectrix")
-    current_enviroment = data["enviroment"]
-    pygame.mouse.set_visible(False)
-    clock = pygame.time.Clock()
+with open("dictionaries/containerDictionaries.json", "r") as f:
+    containers_list = json.load(f)
 
-    spawn_timer = 0
-    base_spawn_delay = 1000
+#[-----------------]#
+#[----VARIABLES----]#
+#[-----------------]#
 
-    display_currency = data["currency"]
+spawn_timer = 0
+base_spawn_delay = 1000
 
-    currency_font = pygame.font.Font("assets/Pixel.ttf", 70)
+autosave_timer = 0
+autosave_interval = 15000
 
-    enviroment_background = pygame.image.load(f"assets/images/enviroments/{current_enviroment}.png")
-    enviroment_background = pygame.transform.scale(enviroment_background, (screen_width, screen_height))
+popups = []
+screen_bugs = []
+container_bugs = []
 
-    in_container_bugs = []
-    on_screen_bugs = []
+clock = pygame.time.Clock()
 
-    ##[ -- Manager Handlers -- ]##
+#[-------------]#
+#[----INIT-----]#
+#[-------------]#
 
-    popup_manager = PopupTextManager()                                                              # --- Popup Manager
-    container_manager = ContainerManager(data["container"], screen_height, in_container_bugs, data, Bug) # --- Container Manager
-    phone_manager = PhoneManager(screen_width, screen_height, container_manager, data, "off")       # --- Phone Manager
-    bug_manager = BugManager(data["enviroment"], enviroment_dictionaries, on_screen_bugs, in_container_bugs, popup_manager)  # --- Bug Manager
-    bugnet_manager = BugnetManager(data["bugnet"], (0, 0))                                          # --- Bugnet Manager
-    upgrade_manager = UpgradeManager(25, -55, popup_manager, data)                                  # --- Upgrade Button Manager
+pygame.init()
+pygame.font.init()
+pygame.mouse.set_visible(False)
+pygame.display.set_caption("Insectrix")
+pygame.display.set_icon(pygame.image.load("assets/images/gameIcon.png"))
 
-    container_manager.loadBugs()
+#[---------------]#
+#[----DISPLAY----]#
+#[---------------]#
 
-    ##[ -- Button Information -- ]##
+BASE_WIDTH = 2560
+BASE_HEIGHT = 1440
 
-    font = pygame.font.Font("assets/rainyhearts.ttf", 35)
-    frame_image = pygame.image.load("assets/ui/buttonFrame.png")
-    upgrade_icons_path = "assets/images/upgradeIcons/"
+screen = pygame.display.set_mode((0, 0), pygame.NOFRAME)
+screen_width, screen_height = screen.get_size()
 
-    def create_container_data(container_type, old_container=None):
-        template = container_dictionaries[container_type]
+scale_x = screen_width / BASE_WIDTH
+scale_y = screen_height / BASE_HEIGHT
 
-        existing_bugs = {}
-        if old_container and "bugs" in old_container:
-            existing_bugs = old_container["bugs"]
+scale = min(scale_x, scale_y)
 
-        return {
-            "type": container_type,
-            "capacity": template["capacity"],
-            "offset": template["offset"],
-            "bugs": existing_bugs
-        }
+def sx(x): return int(x * scale)
+def sy(y): return int(y * scale)
 
-    buttons_list = [
-        UpgradeButton(0, 0, 300, 200, frame_image, pygame.image.load(f"{upgrade_icons_path}sturdy_bugnet_icon.png"),  "Sturdy Bugnet", 20, font, True, effect=lambda v: v.__setitem__("bugnet", "sturdy")),
-        UpgradeButton(0, 0, 300, 200, frame_image, pygame.image.load(f"{upgrade_icons_path}medium_jar_icon.png"), "Medium Jar", 90, font, True, effect=lambda v: v.__setitem__("container", create_container_data("medium_jar", v["container"]))),
-        UpgradeButton(0, 0, 300, 200, frame_image, pygame.image.load(f"{upgrade_icons_path}truck_icon.png"), "Basic Plan", 75, font, True, effect=lambda v: v.__setitem__("sellPlan", "basic")),
-        UpgradeButton(0, 0, 300, 200, frame_image, pygame.image.load(f"{upgrade_icons_path}pollen_icon.png"), "Pollen Bottle", 100, font, False, effect=lambda v: v.__setitem__("spawn_rate", round(v["spawn_rate"] + 0.05, 2))),
-        UpgradeButton(0, 0, 300, 200, frame_image, pygame.image.load(f"{upgrade_icons_path}spraycan_icon.png"), "Florescent Spray", 150, font, False, effect=lambda v: v.__setitem__("max_bugs", v["max_bugs"] + 1))
-    ]
+def scale_position(x, y):
+    return sx(x), sy(y)
 
-    for button in buttons_list:
-        if not data["purchases"].get(button.name, False): # --- If player does not own upgrade, display it
-            upgrade_manager.add_button(button, screen_width)
+def load_scaled(path, width, height):
+    image = pygame.image.load(path).convert_alpha()
+    return pygame.transform.scale(image, (sx(width), sy(height)))
 
-    running = True
+def font(name, size):
+    return pygame.font.Font(f"assets/fonts/{name}.ttf", sx(size))
 
-    #[----------------]#
-    #[----GAMELOOP----]#
-    #[----------------]#
+#[----------------]#
+#[----MANAGERS----]#
+#[----------------]#
 
-    while running:
+environment_manager = EnvironmentManager((screen_width, screen_height), data)
+container_manager = ContainerManager(data["container"]["type"], containers_list, screen, container_bugs, data)
+bug_manager = BugManager(data["environment"], container_bugs, None, bugs_list, environments_list)
+bugnet_manager = BugnetManager(data["bugnet"], bugnets_list)
 
-        ##[ -- Input Handling -- ]##
+#[---------------]#
+#[----OPTIONS----]#
+#[---------------]#
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+options_button = load_scaled("assets/ui/options_button.png", 70, 70)
+options_button_rect = options_button.get_rect(topleft=scale_position(15, 15))
+
+options_open = False
+current_frame = None
+
+stats_button = load_scaled("assets/ui/stats_button.png", 70, 70)
+stats_rect = stats_button.get_rect(topleft=scale_position(15, 100))
+
+settings_button = load_scaled("assets/ui/settings_button.png", 70, 70)
+settings_rect = settings_button.get_rect(topleft=scale_position(15, 185))
+
+quit_button = load_scaled("assets/ui/quit_button.png", 70, 70)
+quit_rect = quit_button.get_rect(topleft=scale_position(15, 270))
+
+options_frame = load_scaled("assets/ui/options_frame.png", 325, 475)
+options_frame_rect = options_frame.get_rect(topleft=scale_position(95, 15))
+
+def load_stats():
+    title = font("ThinBold", 35).render("Stats", False, (255, 255, 255))
+    max_bugs_text = font("Regular", 18).render(f"Max Bugs: {data["max_bugs"]}", True, (255, 255, 255))
+    spawn_rate_text = font("Regular", 18).render(f"Spawn Rate: {data["spawn_rate"]} (s)", True, (255, 255, 255))
+    bugnet_text = font("Regular", 18).render(f"Current Bugnet: {data["bugnet"]}", True, (255, 255, 255))
+    environment_text = font("Regular", 18).render(f"Current Environment: {data["environment"]}", True, (255, 255, 255))
+    sell_plan_text = font("Regular", 18).render(f"Current Sell Plan: {data["sell_plan"]}", True, (255, 255, 255))
+    container_text = font("Regular", 18).render(f"Current Container: {data["container"]["type"]}", True, (255, 255, 255))
+    capacity_text = font("Regular", 18).render(f"Max Capacity: {data["container"]["capacity"]}", True, (255, 255, 255))
+
+    screen.blit(title, title.get_rect(center=scale_position(257.5, 80)))
+    screen.blit(max_bugs_text, max_bugs_text.get_rect(center=scale_position(257.5, 120)))
+    screen.blit(spawn_rate_text, spawn_rate_text.get_rect(center=scale_position(257.5, 150)))
+    screen.blit(bugnet_text, bugnet_text.get_rect(center=scale_position(257.5, 180)))
+    screen.blit(environment_text, environment_text.get_rect(center=scale_position(257.5, 210)))
+    screen.blit(sell_plan_text, sell_plan_text.get_rect(center=scale_position(257.5, 240)))
+    screen.blit(container_text, container_text.get_rect(center=scale_position(257.5, 270)))
+    screen.blit(capacity_text, capacity_text.get_rect(center=scale_position(257.5, 300)))
+
+def load_settings():
+    title = font("ThinBold", 35).render("Settings", False, (255, 255, 255))
+
+    if data["settings"]["sound_effects"]:
+        sound_effects_button = font("Regular", 18).render("Sound Effects: ENABLED", True, (0, 255, 0))
+    else:
+        sound_effects_button = font("Regular", 18).render("Sound Effects: DISABLED", True, (255, 0, 0))
+
+    if data["settings"]["popups"]:
+        popups_button = font("Regular", 18).render("PopUps: ENABLED", True, (0, 255, 0))
+    else:
+        popups_button = font("Regular", 18).render("PopUps: DISABLED", True, (255, 0, 0))
+
+    if data["settings"]["music"]:
+        music_button = font("Regular", 18).render("Music: ENABLED", True, (0, 255, 0))
+    else:
+        music_button = font("Regular", 18).render("Music: DISABLED", True, (255, 0, 0))
+
+    screen.blit(title, title.get_rect(center=scale_position(257.5, 80)))
+    screen.blit(sound_effects_button, sound_effects_button.get_rect(center=scale_position(257.5, 130)))
+    screen.blit(popups_button, popups_button.get_rect(center=scale_position(257.5, 170)))
+    screen.blit(music_button, music_button.get_rect(center=scale_position(257.5, 210)))
+
+#[---------------]#
+#[----RUNNING----]#
+#[---------------]#
+
+running = True
+
+container_manager.load_container(container_bugs, load_scaled, bugs_list, scale, scale_position, Bug, data)
+bugnet_manager.load_bugnet(load_scaled)
+
+current_container = data["container"]["type"]
+current_bugnet = data["bugnet"]
+
+static_surface = pygame.Surface((screen_width, screen_height))
+static_surface.blit(environment_manager.image, (0, 0))
+
+cursor_icon = load_scaled("assets/ui/mouse_cursor.png", 64, 64)
+cursor_icon_rect = cursor_icon.get_rect()
+
+clickable_rects = [
+    ("options", options_button_rect)
+]
+
+while running:
+    mouse_pos = pygame.mouse.get_pos()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            save_game(data)
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            bug_manager.collect_bug(event.pos, pygame.time.get_ticks(), data, container_manager.rect, bugnet_manager, screen_bugs, popups, scale, font, PopupText)
+
+            if options_button_rect.collidepoint(mouse_pos) and not options_open:
+                options_open = True
+            elif options_button_rect.collidepoint(mouse_pos) and options_open:
+                options_open = False
+            elif stats_rect.collidepoint(mouse_pos):
+                if current_frame == None or current_frame == "settings":
+                    current_frame = "stats"
+                else:
+                    current_frame = None
+            elif settings_rect.collidepoint(mouse_pos):
+                if current_frame == None or current_frame == "stats":
+                    current_frame = "settings"
+                else:
+                    current_frame = None
+            elif quit_rect.collidepoint(mouse_pos):
+                save_game(data)
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                bug_manager.collect_bug(event.pos, container_manager.container_image_rect, data, pygame.mouse, bugnet_manager) # --- Bug Clicked
-                upgrade_manager.handle_click(event.pos)                                                                        # --- Button Clicked
-                phone_manager.handle_click(event.pos, data)                                                                    # --- Phone Clicked
-                bugnet_manager.swing(data)                                                                                     # --- Bugnet Swung
 
-        screen.blit(enviroment_background, (0, 0))
-        dt = clock.tick(60) / 1000
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSLASH:
+                data = default_data.copy()
+                save_game(data)
+                popups.append(PopupText((screen_width / 2, screen_height /2), "DATA WIPED", font("Regular", 50), (255, 0, 0)))
 
-        ##[ -- Bug Spawning -- ]##
+                container_manager.load_container(container_bugs, load_scaled, bugs_list, scale, scale_position, Bug, data)
+                bugnet_manager.load_bugnet(load_scaled)
 
-        spawn_delay = base_spawn_delay / data["spawn_rate"]
+    dt = clock.tick(60) / 1000
+    spawn_delay = base_spawn_delay * data["spawn_rate"]
 
+    if len(screen_bugs) != data["max_bugs"]:
         spawn_timer += dt * 1000
 
-        if spawn_timer >= spawn_delay and len(on_screen_bugs) < data["max_bugs"]:
-            bug_manager.spawn_bug(screen_width, screen_height)
-            spawn_timer = 0
+    if spawn_timer >= spawn_delay and len(screen_bugs) < data["max_bugs"]:
+        bug_manager.spawn_bug(screen_width, screen_height, screen_bugs, load_scaled)
+        spawn_timer = 0
 
-        ##[ -- Cursor Icon Handler -- ]##
+    autosave_timer += dt * 1000
+    if autosave_timer >= autosave_interval:
+        save_game(data)
+        autosave_timer = 0
 
-        mouse_pos = pygame.mouse.get_pos()
+    for popup in popups[:]:
+        popup.update(scale)
+        if popup.dead():
+            popups.remove(popup)
 
-        if upgrade_manager.is_hovering(mouse_pos) or phone_manager.is_hovering(mouse_pos):
-            bugnet_manager.visible = False
-        else:
-            bugnet_manager.visible = True
+    new_container = data["container"]["type"]
+    new_bugnet = data["bugnet"]
 
-        ##[ -- Visual Updates -- ]##
+    if current_container != new_container:
+        container_manager.load_container(container_bugs, load_scaled, bugs_list, scale, scale_position, Bug, data)
+    elif current_bugnet != new_bugnet:
+        bugnet_manager.load_bugnet(load_scaled)
 
-        upgrade_manager.draw(screen)                      # --- Upgrade Draw
-        phone_manager.update(screen, dt)                  # --- Phone Update
-        bugnet_manager.update(screen, pygame.mouse, data) # --- Bugnet Update
-        bug_manager.update(screen_width, screen)          # --- Bug Update
-        container_manager.update(screen)                  # --- Container Update
-        popup_manager.update(dt)                          # --- Popup Update
-        popup_manager.draw(screen)                        # --- Popup Draw
+    screen.blit(static_surface, (0, 0))
+    screen.blit(container_manager.image, container_manager.rect)
+    
+    screen.blit(options_button, options_button_rect)
 
-        ##[ -- Currency Text -- ]##
+    if options_open:
+        screen.blit(stats_button, stats_rect)
+        screen.blit(settings_button, settings_rect)
+        screen.blit(quit_button, quit_rect)
 
-        difference = data["currency"] - display_currency
-        last_currency = None
-        step = max(1, abs(difference) // 10)
+        clickable_rects.extend([
+            ("options_frame", options_frame_rect),
+            ("stats", stats_rect),
+            ("settings", settings_rect),
+            ("quit", quit_rect)
+        ])
+    else:
+        current_frame = None
+        clickable_rects = [("options", options_button_rect)]
 
-        if difference > 0:
-            display_currency += step
-        elif difference < 0:
-            display_currency -= step
+    if current_frame != None:
+        screen.blit(options_frame, options_frame_rect)
 
-        if display_currency != last_currency:
-            currency_text = currency_font.render(f"{display_currency} Insectra", True, (255, 255, 255))
-            shadow = currency_font.render(f"{display_currency} Insectra", True, (0, 0, 0))
-            last_currency = display_currency
-        
-        currency_rect = currency_text.get_rect(midtop=(screen_width // 2, 10))
-        screen.blit(shadow, (currency_rect.x + 3, currency_rect.y + 3))
-        screen.blit(currency_text, currency_rect)
-        
-        ##[ -- Display --]##
+    if current_frame == "stats":
+        load_stats()
+    if current_frame == "settings":
+        load_settings()
 
-        pygame.display.flip()
+    bugnet_manager.draw(screen, data, cursor_icon, cursor_icon_rect)
+    bug_manager.draw(screen_width, screen_bugs, screen, scale)
+    container_manager.draw(container_bugs, screen, scale, screen_width)
 
-    pygame.quit()
+    for popup in popups:
+        popup.draw(screen)
 
-##[ -- Initiate Game --]##
+    fps = clock.get_fps()
+    fps_text = font("Regular", 20).render(f"FPS: {int(fps)}", False, (255, 255, 255))
+    screen.blit(fps_text, scale_position(10, 10))
+    
+    bugnet_manager.visible = not any(rect.collidepoint(mouse_pos) for name, rect in clickable_rects)
 
-if __name__ == "__main__":
-    main()
+    pygame.display.flip()
